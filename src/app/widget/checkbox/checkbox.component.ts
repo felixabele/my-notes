54707165c7b05baef46ff45904477cb3f3c0bb0e
Felix Abele <felix.abele@diconium.com>
2022-09-02 08:11:24 +0200

    checkbox-item: editable titles

--- src/app/widget/checkbox/checkbox.component.ts
+++ src/app/widget/checkbox/checkbox.component.ts
@@ -1,13 +1,13 @@
 import { ChecklistItem } from './../widget';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
   template: `
-    <div class="checklist-item">
+    <div class="checklist-item" #checklistItem>
       <mat-checkbox [(ngModel)]="done" (change)="onChange()">
-        {{item.title}}
       </mat-checkbox>
+      <input type="text" [(ngModel)]="title" class="title-input" (blur)="onStopEditing()" />
       <button (click)="onDelete()" mat-icon-button class="checklist-item__delete-button">
         <mat-icon>close</mat-icon>
       </button>
@@ -15,19 +15,28 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   `,
   styles: [
     '.checklist-item__delete-button { float: right; display: none }',
-    '.checklist-item:hover > .checklist-item__delete-button { display: block }'
+    '.checklist-item:hover > .checklist-item__delete-button { display: block }',
+    '.mat-checkbox { margin-right: 1rem; }',
+    '.title-input { border: 0; outline: none }',
   ]
 })
 export class CheckboxComponent implements OnInit {
   @Input() item!: ChecklistItem;
   @Output() update = new EventEmitter<ChecklistItem>();
   @Output() delete = new EventEmitter<ChecklistItem>();
+  @ViewChild('titleInput') titleInput: ElementRef<HTMLInputElement> | undefined;
+  @ViewChild('checklistItem') checklistItem: ElementRef<HTMLInputElement> | undefined;
+
   done: boolean = false;
+  title: string = '';
+  origTitle: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
-    this.done = !!this.item?.done;
+    this.done = !!this.item.done;
+    this.title = this.item.title;
+    this.origTitle = this.item.title;
   }
 
   onChange(): void {
@@ -36,5 +45,11 @@ export class CheckboxComponent implements OnInit {
 
   onDelete(): void {
     this.delete.emit(this.item);
+  }
+
+  onStopEditing(): void {
+    if (this.origTitle !== this.title) {
+      this.update.emit({ ...this.item, title: this.title });
+    }
   }
 }
